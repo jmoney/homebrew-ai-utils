419f360da3a49333babe601a1d2d2661327d4c63
goreleaserbot <noreply@gmail.com>
2024-06-02 22:54:54 -0500

    Brew formula update for tokenizer version 0.3

--- Formula/tokenizer.rb
+++ Formula/tokenizer.rb
@@ -5,13 +5,13 @@
 class Tokenizer < Formula
   desc "CLI for tokenizing text from stdin"
   homepage "https://jmoney.dev/tokenizer-utils"
-  version "0.2"
+  version "0.3"
   license "Apache 2.0"
   depends_on :macos
 
   on_arm do
-    url "https://github.com/jmoney/tokenizer-utils/releases/download/0.2/tokenizer-utils_0.2_darwin_arm64.tar.gz"
-    sha256 "e9b308144ef347bf77122c0fa6f713eeeae49dd47d4718288fde6f411b187554"
+    url "https://github.com/jmoney/tokenizer-utils/releases/download/0.3/tokenizer-utils_0.3_darwin_arm64.tar.gz"
+    sha256 "50f87a1a314434d98e9d85413e27712cdcec00e39798901b0a89e089e45036e4"
 
     def install
       bin.install "tokenizer"
